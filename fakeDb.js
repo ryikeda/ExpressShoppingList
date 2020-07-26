350d1b5f979dd82ae2397a948599f5db019972b5
rafael ubuntu <ryikeda@outlook.com>
2020-07-26 15:18:43 +0900

    created routes (get, post, pacth and delete)

--- fakeDb.js
+++ fakeDb.js
@@ -1,3 +1,8 @@
 global.items = [];
 
+// global.items = [
+//   { name: "item1", price: 1 },
+//   { name: "item2", price: 2 },
+// ];
+
 module.exports = items;
--- routes/items.js
+++ routes/items.js
@@ -3,8 +3,41 @@ const router = new express.Router();
 const ExpressError = require("../expressError");
 const items = require("../fakeDb");
 
-router.get("/", (req, res) => {
+router.get("/", (req, res, next) => {
   res.json({ items });
+});
+
+router.post("/", (req, res, next) => {
+  if (!req.body.name || !req.body.price)
+    throw new ExpressError(
+      "Invalid request please add item's name and price to query string"
+    );
+  const newItem = { name: req.body.name, price: req.body.price };
+  items.push(newItem);
+  res.status(201).json({ added: newItem });
+});
+
+router.get("/:name", (req, res, next) => {
+  const foundItem = items.find((item) => item.name === req.params.name);
+  if (!foundItem) throw new ExpressError("Item not found", 404);
+  res.json({ foundItem });
+});
+
+router.patch("/:name", (req, res, next) => {
+  const foundItem = items.find((item) => (item.name = req.params.name));
+  if (!foundItem) throw new ExpressError("Item not found", 404);
+  foundItem.name = req.body.name;
+  foundItem.price = req.body.price;
+  res.json({ updated: foundItem });
+});
+
+router.delete("/:name", (req, res, next) => {
+  const foundItemIdx = items.findIndex(
+    (itemIdx) => itemIdx.name === req.params.name
+  );
+  if (foundItemIdx === -1) throw new ExpressError("Item not found", 404);
+  items.splice(foundItemIdx, 1);
+  res.json({ message: "Deleted" });
 });
 
 module.exports = router;
